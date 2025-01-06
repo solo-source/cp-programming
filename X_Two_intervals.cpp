30df3d3e7b7786ac7fcc242ea522d85889207138
Samrendra Pratap Singh <153061395+solo-source@users.noreply.github.com>
2025-01-06 17:23:01 +0530

    Solved the question "Two Intervals" from CodeForces.

--- X_Two_intervals.cpp
+++ X_Two_intervals.cpp
@@ -1,7 +1,37 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
 int main()
 {
-       
+    int l1, r1, l2, r2;
+    int lower, upper;
+
+    cin >> l1 >> r1 >> l2 >> r2;
+
+    if (l2 > r1 || l1 > r2)
+    {
+        cout << -1;
+    }
+    else if (l2 >= l1)
+    {
+        lower = l2;
+        cout << lower << " ";
+        if (r1 <= r2)
+            upper = r1;
+        else
+            upper = r2;
+        cout << upper;
+    }
+    else
+    {
+        lower = l1;
+        cout << lower << " ";
+        if (r1 <= r2)
+            upper = r1;
+        else
+            upper = r2;
+        cout << upper;
+    }
+
+    return 0;
 }
