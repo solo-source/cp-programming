2b6684199446b990a371e47ffba123860845db06
Samrendra Pratap Singh <153061395+solo-source@users.noreply.github.com>
2025-01-06 22:47:39 +0530

    Finished with Data types and Conditions.

--- X_Two_intervals.cpp
+++ X_Two_intervals.cpp
@@ -35,3 +35,35 @@ int main()
 
     return 0;
 }
+
+// int main()
+// {
+// 	int l1 = 0, r1 = 0, l2 = 0, r2 = 0, lower = 0, upper = 0;
+
+// 	std::cin >> l1 >> r1 >> l2 >> r2;
+
+// 	if (l2 >= l1 && l2 <= r1)
+// 	{
+// 		lower = l2;
+// 		if (r2 <= r1)
+// 			upper = r2;
+// 		else
+// 			upper = r1;
+// 		std::cout << lower << " " << upper;
+// 	}
+// 	else if (l1 >= l2 && l1 <= r2)
+// 	{
+// 		lower = l1;
+// 		if (r1 <= r2)
+// 			upper = r1;
+// 		else
+// 			upper = r2;
+// 		std::cout << lower << " " << upper;
+// 	}
+// 	else
+// 	{
+// 		std::cout << -1;
+// 	}
+
+// 	return 0;
+// }
