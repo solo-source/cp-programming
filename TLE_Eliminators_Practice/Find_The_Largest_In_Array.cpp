dbd15e5d18028c1a1b877b2522b94ccc7c11c799
Samrendra Pratap Singh <153061395+solo-source@users.noreply.github.com>
2025-03-05 17:18:52 +0530

    Learning with IO Manipulations.

--- TLE_Eliminators_Practice/Find_The_Largest_In_Array.cpp
+++ TLE_Eliminators_Practice/Find_The_Largest_In_Array.cpp
@@ -1,17 +1,43 @@
 #include <iostream>
 
+// int main()
+// {
+//     int arr[5] = {1,34,5,67,45};
+
+//     int max = INT_MIN;
+
+//     for(int i = 0; i < sizeof(arr)/sizeof(arr[0]); i++)
+//     {
+//         if(arr[i] > max)
+//             max = arr[i];
+//     }
+
+//     std::cout << max;
+//     return 0;
+// }
+
 int main()
 {
-    int arr[5] = {1,34,5,67,45};
-    
+    int n;
+    std::cin >> n;
+
+    int a[n];
+    for (int i = 0; i < n; i++)
+    {
+        std::cin >> a[i];
+    }
+
+    // Define INT_MIN as the largest.
     int max = INT_MIN;
 
-    for(int i = 0; i < sizeof(arr)/sizeof(arr[0]); i++)
+    for (int i = 0; i < n; i++)
     {
-        if(arr[i] > max)
-            max = arr[i];
+        // if (a[i] > max)
+        //     max = a[i];
+        max = std::max(max, a[i]);
     }
 
     std::cout << max;
+
     return 0;
 }
